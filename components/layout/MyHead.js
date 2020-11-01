2bc553c2fef3bc2e3b5233b4fa8212fd9f93b49a
Alex Radin <alexradin1964@gmail.com>
2020-11-01 08:41:42 +0300

    test2

--- components/layout/MyHead.js
+++ components/layout/MyHead.js
@@ -5,7 +5,7 @@ export default function MyHead({title}) {
     return (
         <Head>
             <title>{title}</title>
-            <link rel="shortcut icon" href="../../public/favicon.ico"/>
+            <link rel="shortcut icon" href="/favicon.ico"/>
             <link rel="apple-touch-icon" sizes="180x180" href="../../public/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="../../public/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="../../public/favicon-16x16.png"/>
